6436ebd5ab76380a32d40ccfabdbb1ef29344ec7
nimedinasa <33522696+nimedinasa@users.noreply.github.com>
2024-01-15 00:03:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,10 @@
-function toggleSection(sectionId) {
-    // Hide all sections
-    document.querySelectorAll('.fancy-section').forEach(section => {
-        section.style.display = 'none';
-    });
+function showSection(sectionNumber) {
+            // Hide all sections
+            document.querySelectorAll('.fancy-section').forEach(function(section) {
+                section.style.display = 'none';
+            });
 
-    // Show the selected section
-    const selectedSection = document.getElementById(sectionId);
-    if (selectedSection) {
-        selectedSection.style.display = 'flex';
-    }
-}
-
-
-
+            // Show the selected section
+            document.getElementById('section' + sectionNumber).style.display = 'block';
+        }
 
