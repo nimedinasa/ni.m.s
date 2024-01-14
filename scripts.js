4690dc742d46cd900d8e48ba11fdfb91290b17f2
nimedinasa <33522696+nimedinasa@users.noreply.github.com>
2024-01-15 00:11:21 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
- // Hide all sections on page load
-        document.addEventListener("DOMContentLoaded", function() {
-            document.querySelectorAll('.fancy-section').forEach(function(section) {
-                section.style.display = 'none';
-            });
-        });
-
-        function showSection(sectionNumber) {
+function showSection(sectionNumber) {
             // Hide all sections
             document.querySelectorAll('.fancy-section').forEach(function(section) {
                 section.style.display = 'none';
