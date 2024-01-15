89ad95b6ba35b94ae0242646ff76218542681fb9
nimedinasa <33522696+nimedinasa@users.noreply.github.com>
2024-01-15 13:37:26 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 function toggleSection(sectionId) {
     const section = document.getElementById(sectionId);
     const sections = document.querySelectorAll('.fancy-section');
-    
+    document.getElementById('pp').style.display = 'none';
     sections.forEach((s) => {
         s.style.display = 'none';
     });
